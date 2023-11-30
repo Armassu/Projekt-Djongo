06f356f8bf9e77f90f5a10624c5e57af5f2f8f33
Paweł Byrka <gdx154315@student.gdansk.merito.pl>
2023-11-30 22:47:09 +0100

    Poprawiony kod w pliku views.py

--- Django/nieruchomosci/views.py
+++ Django/nieruchomosci/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import nieruchomosci  # Zmieniłem 'YourModel' na 'nieruchomosci'
 
-# Create your views here.
+def home(request):
+    # Pobierz wszystkie obiekty z modelu
+    all_objects = nieruchomosci.objects.all()  # Zmieniłem 'YourModel' na 'nieruchomosci'
+
+    # Pobierz obiekt po konkretnej wartości pola (zmień 'nazwa_pola' na rzeczywistą nazwę pola)
+    specific_object = nieruchomosci.objects.get(nazwa_pola='wartość')  # Zmieniłem 'YourModel' na 'nieruchomosci'
+
+    # Dodaj te dane do kontekstu
+    dane = {
+        'nazwa_zmiennej': 'Wartość zmiennej',
+        'all_objects': all_objects,
+        'specific_object': specific_object,
+    }
+
+    # Renderuj widok, przekazując dane w kontekście
+    return render(request, 'nieruchomosci/home.html', context=dane)
